79347c919d26e49e7e2d4a84289e244c514797d1
Abhis-123 <me19b069@smail.iitm.ac.in>
2021-12-17 21:02:56 +0530

    statement to readme

---
+++ 1_to_100/2/solution.py
@@ -0,0 +1,11 @@
+class ListNode:
+    def __init__(self,val=0,next = None):
+        self.val = val
+        self.next = next
+
+def add_two_numbers(l1,l2):
+    """
+    l1 and l2 contain digits of two numbers in reversed order
+    """
+
+    
